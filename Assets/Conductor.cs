6ad96f6ef891f5d1a6e5db6defd083f0dc789369
Hairyah <alexis.lejeune76@hotmail.fr>
2023-02-28 19:24:44 +0100

    fix : Conductor
    Correction rythme et ajout contre-temps et erreur.

--- Assets/Conductor.cs
+++ Assets/Conductor.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Conductor : MonoBehaviour
 {
+    public bool beatIsUsed;
+
     //BPM de la musique
     public float songBpm;
 
@@ -33,7 +35,9 @@ public class Conductor : MonoBehaviour
         dspSongTime = (float)AudioSettings.dspTime;
 
         musicSource.Play();
-    }
+
+        beatIsUsed = false;
+}
 
     void Update()
     {
@@ -43,11 +47,51 @@ public class Conductor : MonoBehaviour
         //Determine le nombre de beat depuis le début de la chanson
         songPositionInBeats = songPosition / secPerBeat;
 
-
-        if (Input.GetKeyDown("space") && (songPositionInBeats< Mathf.Round(songPositionInBeats)-0.10f || songPositionInBeats < Mathf.Round(songPositionInBeats) + 0.10f))
+        /*if (Input.GetKeyDown("space"))
         {
             Debug.Log("PAN");
+            Debug.Log(songPositionInBeats);
+            Debug.Log(Mathf.Floor(songPositionInBeats));
             //Il me faut une condition pour dire que ça s'active qu'une seule fois
-        } //Il faut un truc pour quand c'est pas en rythme
+        }*/
+
+        /*if (Input.GetKeyDown("space") && (songPositionInBeats >= Mathf.Round(songPositionInBeats) - 0.15f && songPositionInBeats <= Mathf.Round(songPositionInBeats) + 0.15f))
+        {
+            Debug.Log("PAN");
+            Debug.Log(songPositionInBeats);
+            Debug.Log(Mathf.Round(songPositionInBeats));
+            //Il me faut une condition pour dire que ça s'active qu'une seule fois
+        } //Il faut un truc pour quand c'est pas en rythme*/
+
+        if (!beatIsUsed && Input.GetKeyDown("space") && (songPositionInBeats >= Mathf.Floor(songPositionInBeats) + 0.40f && songPositionInBeats <= Mathf.Floor(songPositionInBeats) + 0.60f))
+        {
+            Debug.Log("PAN");
+
+            /*Debug.Log(songPositionInBeats);
+            Debug.Log(Mathf.Floor(songPositionInBeats));*/
+
+            beatIsUsed = true;
+
+            StartCoroutine(RefreshBeat());
+
+            //Il me faut une condition pour dire que ça s'active qu'une seule fois
+        } else if(!beatIsUsed && Input.GetKeyDown("space") && (songPositionInBeats >= Mathf.Round(songPositionInBeats) - 0.10f && songPositionInBeats <= Mathf.Floor(songPositionInBeats) + 0.10f))
+        {
+            Debug.Log("CONTRE-PAN");
+
+            beatIsUsed = true;
+
+            StartCoroutine(RefreshBeat());
+        } else if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Raté !");
+        }
+    }
+
+    IEnumerator RefreshBeat()
+    {
+        yield return new WaitForSeconds(.2f);
+
+        beatIsUsed = false;
     }
 }
